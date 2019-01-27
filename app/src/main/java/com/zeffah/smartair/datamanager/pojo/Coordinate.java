df3e238a9bd1a2aa49a89033ab384e59deb7b3c1
zeffah <zeffah.elly@gmail.com>
2019-01-27 19:18:21 +0300

    -Fixed serializable exception

--- app/src/main/java/com/zeffah/smartair/datamanager/pojo/Coordinate.java
+++ app/src/main/java/com/zeffah/smartair/datamanager/pojo/Coordinate.java
@@ -2,7 +2,9 @@ package com.zeffah.smartair.datamanager.pojo;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Coordinate {
+import java.io.Serializable;
+
+public class Coordinate implements Serializable {
     @SerializedName("Latitude")
     private double latitude;
     @SerializedName("Longitude")
